3a594960411e71f9bf26fd46a9e9167dce8426b4
Ankit Mohokar <31936082+ankitmohokar@users.noreply.github.com>
2018-10-02 17:22:54 -0400

    Update index.js
    Changed sid and auth token

--- index.js
+++ index.js
@@ -3,16 +3,16 @@ var app = express();
 var port = process.env.PORT || 3000;
 
 var twilio = require('twilio');
-var accountSid = 'ACf87d32a10e69a66e4396ce4d73d646c9'; // Your Account SID from www.twilio.com/console
-var authToken = '88b9fc0a47779add9de66f7a4de60ebc';   // Your Auth Token from www.twilio.com/console
+var accountSid = 'YOUR_ACCOUNT_SID'; // Your Account SID from www.twilio.com/console
+var authToken = 'YOUR_AUTH_TOKEN';   // Your Auth Token from www.twilio.com/console
 var client = new twilio(accountSid, authToken);
 
 app.get('/', function(req, res){
 
   client.messages.create({
     body: 'Hello from Ankit Mohokar CS 643 Fall 2018',
-    to: '+19293105441',  // Text this number
-    from: '+17147861082' // From a valid Twilio number
+    to: 'XXXXXXXXXXXX',  // Text this number
+    from: 'XXXXXXXXXXX' // From a valid Twilio number
 })
 .then((message) => {
   console.log(message.sid);
